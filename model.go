9309b784b921711b55d575d0a42c17ccbd1573b0
songzhibin97 <718428482@qq.com>
2023-03-28 15:43:06 +0800

    Update model.go

--- model.go
+++ model.go
@@ -49,6 +49,61 @@ var (
 )
 
 /*
+Feature-specific models
+While the new model is optimized for chat, it works very well for traditional completion tasks. The original GPT-3.5 models are optimized for text completion.gpt-3.5-turbo
+
+Our endpoints for creating embeddings and editing text use their own sets of specialized models.
+*/
+
+/*
+Finding the right model
+Experimenting with is a great way to find out what the API is capable of doing. After you have an idea of what you want to accomplish, you can stay with or another model and try to optimize around its capabilities.gpt-3.5-turbogpt-3.5-turbo
+
+You can use the GPT comparison tool that lets you run different models side-by-side to compare outputs, settings, and response times and then download the data into an Excel spreadsheet.
+*/
+
+/*
+DALL·E Beta
+DALL·E is a AI system that can create realistic images and art from a description in natural language. We currently support the ability, given a prommpt, to create a new image with a certain size, edit an existing image, or create variations of a user provided image.
+
+The current DALL·E model available through our API is the 2nd iteration of DALL·E with more realistic, accurate, and 4x greater resolution images than the original model. You can try it through the our Labs interface or via the API.
+*/
+
+/*
+Whisper Beta
+Whisper is a general-purpose speech recognition model. It is trained on a large dataset of diverse audio and is also a multi-task model that can perform multilingual speech recognition as well as speech translation and language identification. The Whisper v2-large model is currently available through our API with the model name.whisper-1
+
+Currently, there is no difference between the open source version of Whisper and the version available through our API. However, through our API, we offer an optimized inference process which makes running Whisper through our API much faster than doing it through other means. For more technical details on Whisper, you can read the paper.
+*/
+
+var (
+	Whisper = ModelName{Name: "whisper-1"}
+)
+
+/*
+Embeddings
+Embeddings are a numerical representation of text that can be used to measure the relateness between two pieces of text. Our second generation embedding model, is a designed to replace the previous 16 first-generation embedding models at a fraction of the cost. Embeddings are useful for search, clustering, recommendations, anomaly detection, and classification tasks. You can read more about our latest embedding model in the announcement blog post. text-embedding-ada-002
+*/
+
+var (
+	TextEmbeddingAda002 = ModelName{Name: "text-embedding-ada-002"}
+)
+
+/*
+The Moderation models are designed to check whether content complies with OpenAI's usage policies. The models provide classification capabilities that look for content in the following categories: hate, hate/threatening, self-harm, sexual, sexual/minors, violence, and violence/graphic. You can find out more in our moderation guide.
+
+Moderation models take in an arbitrary sized input that is automatically broken up to fix the models specific context window.
+
+MODEL	DESCRIPTION
+text-moderation-latest	Most capable moderation model. Accuracy will be slighlty higher than the stable model
+text-moderation-stable	Almo
+*/
+var (
+	TextModerationLatest = ModelName{Name: "text-moderation-latest"}
+	TextModerationStable = ModelName{Name: "text-moderation-stable"}
+)
+
+/*
 GPT-3 models can understand and generate natural language. These models were superceded by the more powerful GPT-3.5 generation models. However, the original GPT-3 base models (, , , and ) are current the only models that are available to fine-tune.davincicurieadababbage
 
 LATEST MODEL	DESCRIPTION	MAX REQUEST	TRAINING DATA
@@ -69,4 +124,26 @@ var (
 	Curie          = ModelName{Name: "curie"}
 	Babbage        = ModelName{Name: "babbage"}
 	Ada            = ModelName{Name: "ada"}
+)
+
+/*
+The Codex models are now deprecated. They were descendants of our GPT-3 models that would understand and generate code. Their training data contains both natural language and billions of lines of public code from GitHub. Learn more.
+
+They’re most capable in Python and proficient in over a dozen languages including JavaScript, Go, Perl, PHP, Ruby, Swift, TypeScript, SQL, and even Shell.
+
+The following Codex models are now deprecated:
+
+LATEST MODEL	DESCRIPTION	MAX TOKENS	TRAINING DATA
+code-davinci-002	Most capable Codex model. Particularly good at translating natural language to code. In addition to completing code, also supports inserting completions within code.	8,001 tokens	Up to Jun 2021
+code-davinci-001	Earlier version of code-davinci-002	8,001 tokens	Up to Jun 2021
+code-cushman-002	Almost as capable as Davinci Codex, but slightly faster. This speed advantage may make it preferable for real-time applications.	Up to 2,048 tokens
+code-cushman-001	Earlier version of code-cushman-002	Up to 2,048 tokens
+For more, visit our guide on working with Codex.
+*/
+
+var (
+	//CodeDavinci002 = ModelName{Name: "code-davinci-002"}
+	CodeDavinci001 = ModelName{Name: "code-davinci-001"}
+	CodeCushman002 = ModelName{Name: "code-cushman-002"}
+	CodeCushman001 = ModelName{Name: "code-cushman-001"}
 )
